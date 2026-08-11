25d0bded0126e8c1f6ff19d1b8461b6835b4e4cb
Aniketchavan7 <caniket975@gmail.com>
2026-08-11 10:05:48 +0530

    day1

---
+++ Primitive and reference types/Creating a primitive/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // put your code here
+        int year=1996;
+        System.out.println(year);
+
+    }
+}
